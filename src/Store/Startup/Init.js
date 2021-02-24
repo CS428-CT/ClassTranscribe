574215e0bbdae25eaf28d9606bd67b8e69649c7f
Matthew Walowski <mattwalowski@gmail.com>
2021-02-24 07:10:00 -0600

    Add CodeQL (#4)
    * Initial boilerplat commit
    
    * Change gradle version
    
    * Update readme
    
    * Run linter
    
    * Run linter
    
    * Add husky hook
    
    * Update husky files
    
    * Remove husky hook
    
    * Add CodeQL
    
    * Create frontend workflow
    
    * Fix typo
    
    * Create build-android command
    
    * use yarn command to build
    
    * Add build tests
    
    * Remove keystore
    
    * Fix keystore gen
    
    * Change gradlew perms
    
    * Remove format test
    
    * Create empty test
    
    * Update eslint settings
    
    * Build ios in root
    
    * Remove unnessecary components
    
    * Fix linter errors
    
    * Remove build ios test

--- src/Store/Startup/Init.js
+++ src/Store/Startup/Init.js
@@ -3,13 +3,14 @@ import {
   buildAsyncActions,
   buildAsyncReducers,
 } from '@thecodingmachine/redux-toolkit-wrapper'
+import { navigateAndSimpleReset } from '@/Navigators/Root'
 
 export default {
   initialState: buildAsyncState(),
   action: buildAsyncActions('startup/init', async () => {
     await new Promise((resolve) => setTimeout(resolve, 1000))
     // Put code here that does initialization before app is displayed
-    // navigateAndSimpleReset('Main')
+    navigateAndSimpleReset('Main')
   }),
   reducers: buildAsyncReducers({ itemKey: null }), // We do not want to modify some item by default
 }
