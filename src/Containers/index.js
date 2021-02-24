df79b5bc267ec934b55d7436ce27760fb3bd06cc
mlw6 <mattwalowski@gmail.com>
2021-02-24 06:58:00 -0600

    Fix lint errors

--- src/Containers/index.js
+++ src/Containers/index.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 export { default as HomeContainer } from './Home/Index'
-=======
-export { default as IndexExampleContainer } from './Example/Index'
->>>>>>> 3358100dd1e25d1f91490fcb0d3ef6ad0f1b8088
 export { default as IndexStartupContainer } from './Startup/Index'
