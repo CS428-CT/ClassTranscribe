dfa1f16a664af69924d404878946d8149e8a9467
suxianghan <suxiang2@illinois.edu>
2021-03-13 19:31:50 -0600

    Merging tabs, solving issue from video branch

--- App.js
+++ App.js
@@ -1,21 +1,6 @@
 import React, { useState } from 'react'
-// import { StyleSheet } from 'react-native'
 import LoginContainer from './src/containers/LoginContainer/LoginContainer'
 import CTNavigationContainer from './src/containers/CTNavigationContainer/CTNavigationContainer'
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   title: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     marginTop: '30%',
-//   },
-// })
 
 export default function App() {
   const [isUserAuthenticated, setIsUserAuthenticated] = useState(false)
@@ -25,7 +10,10 @@ export default function App() {
   }
 
   const renderApplication = () => {
-    if (isUserAuthenticated) return <CTNavigationContainer />
+    if (isUserAuthenticated)
+      return (
+        <CTNavigationContainer/>
+      )
 
     return <LoginContainer onAuthLevelChange={onAuthLevelChange} />
   }
--- src/containers/CTNavigationContainer/CTNavigationContainer.js
+++ src/containers/CTNavigationContainer/CTNavigationContainer.js
@@ -1,16 +1,23 @@
 import * as React from 'react'
+import { View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Home from '../HomeContainers/Home'
+import VideoContainer from '../VideoContainer/VideoContainer'
+import VideoStyle from '../VideoContainer/VideoContainer.style'
 
 const Tab = createBottomTabNavigator()
 
+function VideoView(){
+    return <View style={VideoStyle.container}><VideoContainer /></View> 
+} 
 const CTNavigationContainer = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Settings" component={Home} />
+
+        <Tab.Screen name="Video" component={VideoView} />
       </Tab.Navigator>
     </NavigationContainer>
   )
