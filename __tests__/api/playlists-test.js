c7f73033616ec6e1a728dd9915f35172bd3bd52a
mlw6 <mattwalowski@gmail.com>
2021-03-19 14:22:45 -0500

    Test video by playlist

--- __tests__/api/playlists-test.js
+++ __tests__/api/playlists-test.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import { format } from '../../src/utils/string'
-import { ENDPOINTS, getPlaylistsByOffering } from '../../src/api/playlists'
+import { ENDPOINTS, getPlaylistsByOffering, getVideosByPlaylist } from '../../src/api/playlists'
 import { HTTP_STATUS_CODES } from '../../src/api'
-import { PLAYLISTS_BY_OFFERING_RESPONSE } from '../mock_responses/mock-playlists-response'
+import { PLAYLISTS_BY_OFFERING_RESPONSE, VIDEOS_BY_PLAYLIST_RESPONSE } from '../mock_responses/mock-playlists-response'
 
 const mock = new MockAdapter(axios)
 describe('Get playlists by offering', () => {
@@ -33,5 +33,35 @@ describe('Get playlists by offering', () => {
       .reply(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR, PLAYLISTS_BY_OFFERING_RESPONSE)
     const playlists = await getPlaylistsByOffering(offeringId)
     expect(playlists).toBe(null)
+  })
+})
+
+describe('Get videos by playlists', () => {
+  const playlistId = '51519746-aa6c-485c-9894-549959c457b5'
+
+  afterEach(async () => {
+    mock.reset()
+  })
+
+  test('when successful', async () => {
+    mock
+      .onGet(`${format(ENDPOINTS.VIDEOS_BY_PLAYLIST, playlistId)}`)
+      .reply(HTTP_STATUS_CODES.OK, VIDEOS_BY_PLAYLIST_RESPONSE)
+    const videos = await getVideosByPlaylist(playlistId)
+    expect(videos).toStrictEqual(VIDEOS_BY_PLAYLIST_RESPONSE)
+  })
+
+  test('when network error', async () => {
+    mock.onGet(`${format(ENDPOINTS.VIDEOS_BY_PLAYLIST, playlistId)}`).networkError()
+    const videos = await getVideosByPlaylist(playlistId)
+    expect(videos).toBe(null)
+  })
+
+  test('when bad status code', async () => {
+    mock
+      .onGet(`${format(ENDPOINTS.VIDEOS_BY_PLAYLIST, playlistId)}`)
+      .reply(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR, PLAYLISTS_BY_OFFERING_RESPONSE)
+    const videos = await getPlaylistsByOffering(playlistId)
+    expect(videos).toBe(null)
   })
 })
--- src/api/playlists.js
+++ src/api/playlists.js
@@ -4,10 +4,25 @@ import { format } from '../utils/string'
 
 export const ENDPOINTS = {
   PLAYLISTS_BY_OFFERING: `${BASE_URL}Playlists/ByOffering/{0}`,
+  VIDEOS_BY_PLAYLIST: `${BASE_URL}Playlists/{0}`,
 }
 
 export const getPlaylistsByOffering = async (offeringId) => {
   const url = format(ENDPOINTS.PLAYLISTS_BY_OFFERING, offeringId)
+
+  try {
+    const resp = await axios.get(url)
+    if (resp?.status !== HTTP_STATUS_CODES.OK) return null
+    return resp.data
+  } catch (error) {
+    console.log(error)
+  }
+
+  return null
+}
+
+export const getVideosByPlaylist = async (playlistId) => {
+  const url = format(ENDPOINTS.VIDEOS_BY_PLAYLIST, playlistId)
 
   try {
     const resp = await axios.get(url)
