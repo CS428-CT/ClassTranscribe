4a8eb75e977cfc4f180fa9380d2d3974d23aa6fd
mlw6 <mattwalowski@gmail.com>
2021-03-22 12:14:56 -0500

    Test clicking on playlist

--- __tests__/frontend/CoursePlaylistsContainer-test.js
+++ __tests__/frontend/CoursePlaylistsContainer-test.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
 import React from 'react'
 import MockAdapter from 'axios-mock-adapter'
-import { render, waitFor } from '@testing-library/react-native'
+import { fireEvent, render, waitFor } from '@testing-library/react-native'
 import { ENDPOINTS } from '../../src/api/playlists'
 import { HTTP_STATUS_CODES } from '../../src/api'
 import { PLAYLISTS_BY_OFFERING_RESPONSE } from '../mock_responses/mock-playlists-response'
 import { format } from '../../src/utils/string'
 import CoursePlaylistsContainer from '../../src/containers/CoursePlaylistsContainer/CoursePlaylistsContainer'
+import { STACK_SCREENS } from '../../src/containers/CTNavigationContainer'
+import { FILE_SERVER_BASE_URL } from '../../src/constants'
 
 const mock = new MockAdapter(axios)
 describe('Check playlists rendering', () => {
@@ -47,3 +49,25 @@ describe('Check playlists rendering', () => {
     expect(playlists.length).toBe(0)
   })
 })
+
+
+describe('Check playlists navigation', () => {
+  const offeringId = 'ac5b1727-629c-443b-8c1a-cc1bd541af6a'
+  const mockNaivgator = {push: jest.fn((screenName, params) => {})}
+
+  test('when clicking on first item', async () => {
+    mock
+      .onGet(`${format(ENDPOINTS.PLAYLISTS_BY_OFFERING, offeringId)}`)
+      .reply(HTTP_STATUS_CODES.OK, [PLAYLISTS_BY_OFFERING_RESPONSE[0]])
+
+    const { queryAllByA11yRole } = render(<CoursePlaylistsContainer courseId={offeringId} navigation={mockNaivgator} />)
+    const playlists = await waitFor(() => queryAllByA11yRole('button'))
+    expect(playlists.length).not.toBe(0)
+
+    fireEvent.press(playlists[0])
+    const expectedPlaylistId = PLAYLISTS_BY_OFFERING_RESPONSE[0].id;
+
+    expect(mockNaivgator.push).toHaveBeenCalled();
+    expect(mockNaivgator.push).toHaveBeenCalledWith(STACK_SCREENS.PLAYLIST, { playlistId: expectedPlaylistId } )
+  })
+});
