d76def15c72278343a7180bdaa4337592feae7ea
mlw6 <mattwalowski@gmail.com>
2021-03-22 10:46:46 -0500

    Test CoursePlaylist rendering

--- __tests__/frontend/CoursePlaylistsContainer-test.js
+++ __tests__/frontend/CoursePlaylistsContainer-test.js
@@ -4,12 +4,11 @@ import MockAdapter from 'axios-mock-adapter'
 import { ENDPOINTS } from '../../src/api/playlists'
 import { HTTP_STATUS_CODES } from '../../src/api'
 import { PLAYLISTS_BY_OFFERING_RESPONSE } from '../mock_responses/mock-playlists-response'
-import { render } from '@testing-library/react-native'
+import { render, waitFor } from '@testing-library/react-native'
 import { format } from '../../src/utils/string'
 import CoursePlaylistsContainer from '../../src/containers/CoursePlaylistsContainer/CoursePlaylistsContainer'
 
 const mock = new MockAdapter(axios)
-
 describe('Check playlists rendering', () => {
     const offeringId = 'ac5b1727-629c-443b-8c1a-cc1bd541af6a'
 
@@ -17,14 +16,15 @@ describe('Check playlists rendering', () => {
         mock.reset();
     })
 
-    test('when given many playlists', () => {
+    test('when given many playlists', async () => {
         mock
         .onGet(`${format(ENDPOINTS.PLAYLISTS_BY_OFFERING, offeringId)}`)
         .reply(HTTP_STATUS_CODES.OK, PLAYLISTS_BY_OFFERING_RESPONSE)
 
         const { queryByText } = render(<CoursePlaylistsContainer courseId={offeringId} />)
-        PLAYLISTS_BY_OFFERING_RESPONSE.forEach((playlist) => {
-            const playlistItem = queryByText(playlist.name);
+
+        PLAYLISTS_BY_OFFERING_RESPONSE.forEach(async (playlist) => {
+            const playlistItem = await waitFor(() => queryByText(playlist.name));
             expect(playlistItem).not.toBe(null);
         });
 
