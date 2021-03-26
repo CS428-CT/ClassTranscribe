95fcff2159ce63e023832eef832b95c6d6178ece
suxianghan <suxiang2@illinois.edu>
2021-03-25 21:56:31 -0500

    Implement basic video showcase

--- src/api/recommend.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import { HTTP_STATUS_CODES, BASE_URL } from '.'
-import { format } from '../utils/string'
-import { getVideosByPlaylist } from './playlists'
-
-export const ENDPOINTS = {
-  PLAYLISTS_BY_OFFERING: `${BASE_URL}Playlists/ByOffering/{0}`,
-  VIDEOS_BY_PLAYLIST: `${BASE_URL}Playlists/{0}`,
-}
--- src/components/Recommend/Recommend.js
+++ src/components/Recommend/Recommend.js
@@ -2,21 +2,40 @@ import React, { useEffect, useState }from "react";
 import RNPickerSelect from "react-native-picker-select";
 import { StyleSheet, Text, View } from "react-native";
 import { getPlaylistsByOffering } from '../../api/playlists'
+import { getVideosByPlaylist } from '../../api/playlists'
 const Recommend = ({ courseId }) => {
     const [playlists, setPlaylists] = useState([])
-    const [selected, setSelected] = useState([])
-
+    const [selected, setSelected] = useState(null)
+    const [videos, setVideos] = useState([])
+    /**
+     * Use Effect monitor playlist fetched
+     */
     useEffect(() => {
         const fetchPlaylists = async () => {
             let response = await getPlaylistsByOffering(courseId)
             if (!response) return
             setPlaylists(response)
         }
-
         fetchPlaylists()
-
     }, [courseId, setPlaylists])
     
+
+    /**
+     * Use Effect monitor Selected playlist and fetched corresbonding video
+     */
+    useEffect(() => {
+        const fetchVideos = async () => {
+            console.log("CHOOSINGS: ", selected)
+            const response = await getVideosByPlaylist(selected)
+            if (!response) return
+            const indexedVid = response.medias.sort((a, b) => a.index - b.index)
+            setVideos(indexedVid)
+        }
+        if(selected){
+            fetchVideos()   
+        }
+    }, [selected, setVideos])
+
     /**
      * Function to Render to playlist picker for reccomend video
      */
@@ -24,22 +43,42 @@ const Recommend = ({ courseId }) => {
         const pickItems = playlists.map(item => {
             return { label: item.name, value: item.id};
         });
-        console.log("ATTEMTION")
-        console.log(pickItems)
-        return(
-            <RNPickerSelect
-                style = {{placeholder:{color:"black"}}}
-                placeholder={pickItems[0]}
-                onValueChange={(value) => console.log(value)}
-                items={pickItems.slice(1)}
-            />
-        )
+        // console.log(pickItems[0]["label"])
+        if(pickItems.length > 0){
+            return(
+                <RNPickerSelect
+                    style = {{placeholder:{color:"black"}}}
+                    placeholder={{
+                        label: pickItems[0]["label"], 
+                        value: pickItems[0]["value"]
+                    }}
+                    onValueChange={(value) => setSelected(value)}
+                    items={pickItems.slice(1)}
+                />
+            )
+        }
+    }
+
+    /**
+     * Function to Render recommend video of given playlist
+     */
+     const renderVideo = () => {
+        const pickItems = videos.map(item => {
+            // console.log(item.name);
+            // console.log(item.watchHistory);
+        });
+        if(videos.length > 0){
+            return(
+                <View></View>
+            )
+        }
     }
 
     return (
         <View style={styles.container}>
             <Text>Test Picker</Text>
             {renderPicker()}
+            {renderVideo()}
         </View>
     );
 }
