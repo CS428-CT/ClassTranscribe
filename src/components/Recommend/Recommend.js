555bf2000081de086f24b6ebacf9c12e99d04dc6
Suxiang Han <suxiang2@illinois.edu>
2021-03-27 18:06:36 -0500

    Fix eslint error in merge

--- src/components/Recommend/Recommend.js
+++ src/components/Recommend/Recommend.js
@@ -1,3 +1,4 @@
+/* eslint prefer-destructuring: ["error", {AssignmentExpression: {array: false}}] */
 import React, { useEffect, useState } from 'react'
 import RNPickerSelect from 'react-native-picker-select'
 import { Text, View } from 'react-native'
@@ -112,7 +113,7 @@ const Recommend = ({ navigation, courseId, mode }) => {
       } else {
         for (let i = videos.length - 1; i >= 0; i -= 1) {
           if (videos[i].watchHistory) {
-            if (i == videos.length - 1) {
+            if (i === videos.length - 1) {
               stat = status.FINISH
             } else {
               rec = videos[i + 1]
@@ -121,7 +122,7 @@ const Recommend = ({ navigation, courseId, mode }) => {
             break
           }
         }
-        if(stat == status.PROCESS){
+        if (stat === status.PROCESS) {
           rec = videos[0]
           stat = status.NORMAL
         }
