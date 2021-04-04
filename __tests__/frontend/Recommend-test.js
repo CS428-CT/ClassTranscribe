6ceabae8eb11d8098b435f06567c84fdf0c2c309
mlw6 <mattwalowski@gmail.com>
2021-04-04 12:36:13 -0500

    Move mock setup into afterEach

--- __tests__/frontend/Recommend-test.js
+++ __tests__/frontend/Recommend-test.js
@@ -15,10 +15,11 @@ const offeringId = 'ac5b1727-629c-443b-8c1a-cc1bd541af6a'
 const playlistId = '51519746-aa6c-485c-9894-549959c457b5'
 const mock = new MockAdapter(axios)
 
-const assertNothingRendered = async (numVideosToCheck) => {
+const assertFirstNVideosNotRendered = async (numVideosToCheck) => {
   const { queryByText } = render(
     <Reccomend courseId={offeringId} mode={false} />
   )
+
   expect(await waitFor(() => queryByText('dddd'))).toBe(null)
   expect(await waitFor(() => queryByText('Placeholder'))).toBe(null)
   expect(await waitFor(() => queryByText('ERR: Unexpected case occur'))).toBe(null)
@@ -38,47 +39,40 @@ describe('Check videos rendering -- mode false', () => {
 
   test('when given many Videos', async () => {
     mock
-      .onGet(`${format(ENDPOINTS.PLAYLISTS_BY_OFFERING, offeringId)}`)
-      .reply(HTTP_STATUS_CODES.OK, PLAYLISTS_BY_OFFERING_RESPONSE)
       .onGet(`${format(ENDPOINTS.VIDEOS_BY_PLAYLIST, playlistId)}`)
       .reply(HTTP_STATUS_CODES.OK, VIDEOS_BY_PLAYLIST_RESPONSE)
 
-    await assertNothingRendered(VIDEOS_BY_PLAYLIST_RESPONSE.length - 1);
+    await assertFirstNVideosNotRendered(VIDEOS_BY_PLAYLIST_RESPONSE.length - 1);
   })
 
   test('when given no Videos', async () => {
     mock
-      .onGet(`${format(ENDPOINTS.PLAYLISTS_BY_OFFERING, offeringId)}`)
-      .reply(HTTP_STATUS_CODES.OK, PLAYLISTS_BY_OFFERING_RESPONSE)
       .onGet(`${format(ENDPOINTS.VIDEOS_BY_PLAYLIST, playlistId)}`)
       .reply(HTTP_STATUS_CODES.OK, [])
 
-    await assertNothingRendered(VIDEOS_BY_PLAYLIST_RESPONSE.length - 1);
+    await assertFirstNVideosNotRendered(VIDEOS_BY_PLAYLIST_RESPONSE.length - 1);
   })
 })
 
 describe('Check videos rendering -- mode true', () => {
   afterEach(() => {
     mock.reset()
+    mock.onGet(`${format(ENDPOINTS.PLAYLISTS_BY_OFFERING, offeringId)}`)
+      .reply(HTTP_STATUS_CODES.OK, PLAYLISTS_BY_OFFERING_RESPONSE)
   })
 
   test('when given many Videos', async () => {
-    mock
-      .onGet(`${format(ENDPOINTS.PLAYLISTS_BY_OFFERING, offeringId)}`)
-      .reply(HTTP_STATUS_CODES.OK, PLAYLISTS_BY_OFFERING_RESPONSE)
-      .onGet(`${format(ENDPOINTS.VIDEOS_BY_PLAYLIST, playlistId)}`)
+      mock.onGet(`${format(ENDPOINTS.VIDEOS_BY_PLAYLIST, playlistId)}`)
       .reply(HTTP_STATUS_CODES.OK, VIDEOS_BY_PLAYLIST_RESPONSE)
 
-    await assertNothingRendered(VIDEOS_BY_PLAYLIST_RESPONSE.length - 2)
+    await assertFirstNVideosNotRendered(VIDEOS_BY_PLAYLIST_RESPONSE.length - 2)
   })
 
   test('when given no Videos', async () => {
     mock
-      .onGet(`${format(ENDPOINTS.PLAYLISTS_BY_OFFERING, offeringId)}`)
-      .reply(HTTP_STATUS_CODES.OK, PLAYLISTS_BY_OFFERING_RESPONSE)
       .onGet(`${format(ENDPOINTS.VIDEOS_BY_PLAYLIST, playlistId)}`)
       .reply(HTTP_STATUS_CODES.OK, [])
 
-    await assertNothingRendered(VIDEOS_BY_PLAYLIST_RESPONSE.length - 1)
+    await assertFirstNVideosNotRendered(VIDEOS_BY_PLAYLIST_RESPONSE.length - 1)
   })
 })
