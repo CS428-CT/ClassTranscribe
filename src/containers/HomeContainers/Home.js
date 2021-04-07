d9eb64752b472b6214b74399d1b111778b8a4d5b
Akshaya <jagaskak@gmail.com>
2021-04-06 21:47:33 -0700

    trying to add university dropdown to Home container

--- src/containers/HomeContainers/Home.js
+++ src/containers/HomeContainers/Home.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import { TouchableNativeFeedback, Switch, FlatList, View, Text } from 'react-native'
+// import { Picker } from '@react-native-picker/picker';
 import PropTypes from 'prop-types'
 import { getStarredOfferingsData, getOfferingsData } from '../../api/offerings'
+// import { getCurrentAuthenticatedUser } from '../../api/auth'
 import CourseCard from '../../components/Cards/CourseCard'
 import Recommend from '../../components/Recommend/Recommend'
 import { STACK_SCREENS } from '../CTNavigationContainer/index'
 import styles from './Home.style'
 
 /**
- * Contains the home screen of the application. Lists starred courses and gives the user the ability
+ * Contains the home screen of the application. Lists courses and gives the user the ability
  * to search for courses. Clicking on a course shows the playlists for it.
  */
 const Home = ({ navigation }) => {
   const [courses, setCourses] = useState([])
-
   useEffect(() => {
     const fetchCourseInfo = async () => {
       const offerings = await getOfferingsData()
@@ -59,7 +60,37 @@ const Home = ({ navigation }) => {
   }
 
   /**
-   * Renders all of the users' starred courses into a FlatList
+   * Render user's login information
+   */
+  // const [universities, setSelectedUniversity] = useState([]);
+  // useEffect(() => {
+  //   const fetchUniversityInfo = async () => {
+  //     const allUniversities = await getUniversities()
+  //     setSelectedUniversity(allUniversities)
+  //   }
+  //   fetchUniversityInfo()
+  // }, [setSelectedUniversity])
+
+  // const renderUniversityDropDown = () => {
+  //     const currentUser = getCurrentAuthenticatedUser()
+  //     const universityId = currentUser.universityId
+
+  //     console.log("WERE HERE!!!!!!!!!!!!!!")
+  //     console.log(universities)
+  //     const universityItems = universities.map( (uni) => {
+  //       return <Picker.Item value={uni.name} label={uni.id}/>
+  //     })
+  //     return (<Picker
+  //             selectedValue={ universityId }
+  //             onValueChange={(university) => setSelectedUniversity(university)}
+  //             mode='dropdown'>
+  //               { universityItems }
+  //     </Picker>)
+  //   // return <Text>{ universityId }</Text>
+  // }
+
+  /**
+   * Renders all of the users' courses into a FlatList
    */
   const renderStarredCourses = () => {
     if (courses == null) return null
@@ -84,6 +115,7 @@ const Home = ({ navigation }) => {
 
   return (
     <View style={styles.viewStyle}>
+      {/* {renderUniversityDropDown()} */}
       {renderSwitch()}
       {renderStarredCourses()}
     </View>
