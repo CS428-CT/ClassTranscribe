dfe915d0440d938a6356fb33220f0b9ba715d282
Suxiang Han <suxiang2@illinois.edu>
2021-04-08 12:43:47 -0500

    Temporary commit for debugging

---
+++ __tests__/frontend/VideoContainer-test.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import React from 'react'
+import MockAdapter from 'axios-mock-adapter'
+import { fireEvent, render, waitFor } from '@testing-library/react-native'
+import { format } from '../../src/utils/string'
+import { ENDPOINTS } from '../../src/api/universities'
+import { HTTP_STATUS_CODES } from '../../src/api'
+import { DEPARTMENTS_RESPONSE } from '../mock_responses/mock-department-response'
+import { VideoContainer } from '../../src/containers/VideoContainer/VideoContainer'
+
+describe('Check rendering', () => {
+  afterEach(() => {
+    mock.reset()
+  })
+
+  test('Check that all show up', async () => {
+    mock
+      .onGet(`${format(ENDPOINTS.DEPARTMENTS, universityId)}`)
+      .reply(HTTP_STATUS_CODES.OK, DEPARTMENTS_RESPONSE)
+
+    const { queryByText, queryAllByA11yRole } = render(
+      <VideoContainer videos={VIDEOS_BY_PLAYLIST_RESPONSE.media} index={0} />
+    )
+  })
+})
